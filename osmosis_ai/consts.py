c0868ecb8573d822e86f6ceca6456bb82819d5a4
jaketrock <jgh@geocrypt.me>
2025-03-24 18:56:22 -0400

    bump version to 0.1.6 in pyproject.toml and consts.py

--- osmosis_ai/consts.py
+++ osmosis_ai/consts.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 # Extract package metadata
 package_name = "osmosis-ai"
-package_version = "0.1.5"
+package_version = "0.1.6"
 
 indent = 2  # Number of spaces to use for indentation in pretty print
 osmosis_api_url = "https://ftgrv77m9f.execute-api.us-west-2.amazonaws.com/prod"
