487dc2543ee3aa68e845690ae5b9c10f110c2d6a
jaketrock <jgh@geocrypt.me>
2025-03-28 13:16:55 -0400

    increment version

--- osmosis_ai/consts.py
+++ osmosis_ai/consts.py
@@ -2,7 +2,7 @@ from enum import Enum
 
 # Extract package metadata
 package_name = "osmosis-ai"
-package_version = "0.1.6"
+package_version = "0.1.7"
 
 indent = 2  # Number of spaces to use for indentation in pretty print
 osmosis_api_url = "https://osmosis.gulp.dev"
