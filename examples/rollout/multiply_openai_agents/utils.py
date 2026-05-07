e6833b23d040e828149443a7d021a5f4af962999
JoyboyBrian <gzybrian@gmail.com>
2026-05-07 11:51:32 -0700

    chore: remove stale OpenAI agents example

--- examples/rollout/multiply_openai_agents/utils.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def extract_solution(solution_str):
-    solution = re.search(r"####\s*([-+]?\d*\.?\d+)", solution_str)
-    if not solution:
-        return None
-    return solution.group(1)
