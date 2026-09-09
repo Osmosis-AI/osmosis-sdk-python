ff402f616ed0b4cf33247c233490c23bcae3b784
Mathew Han <mathewjhan@gmail.com>
2026-09-08 21:37:32 -0700

    [feat] add awaitable rollout handle

--- osmosis_ai/rollout/client/__init__.py
+++ osmosis_ai/rollout/client/__init__.py
@@ -1,11 +1,13 @@
 from osmosis_ai.rollout.client.client import (
     RolloutAdmissionTimeoutError,
     RolloutClient,
+    RolloutHandle,
     RolloutProtocolError,
 )
 
 __all__ = [
     "RolloutAdmissionTimeoutError",
     "RolloutClient",
+    "RolloutHandle",
     "RolloutProtocolError",
 ]
